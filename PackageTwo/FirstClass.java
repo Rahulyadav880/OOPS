dd5496f5f6d70602a1d288fee63a45ea5cde61ab
Rahul Yadav <ry9064526713@gmail.com>
2026-03-30 07:20:57 +0530

    learned how can we use a non-static function inside a static one

--- PackageTwo/FirstClass.java
+++ PackageTwo/FirstClass.java
@@ -9,7 +9,23 @@ public class FirstClass {
     // we can access static variable before creating an object.
     // That's why we make main function as static
     public static void main(String[] args){
-        
+    //we can't call/use a non-static function inside a static function
+      //  greet(); it will give error
+      //but there is a way to access/call/use it - by creating an object for it.
+
+      FirstClass f1 = new FirstClass();
+      //since the greet function is defined in the FirstClass class
+      //and we know that to access a method defined in a class can be accessed
+      //by creating an object of it
+      f1.greet();
+    }
+
+    void greet2(){//now if we have to use greet and greet2 in the main function
+        greet();// then we simply need only one object that will be for greet2 only
+    }
+
+    void greet(){
+        System.out.println("hey, I am non-static");
     }
 
     public static void greetings(){
