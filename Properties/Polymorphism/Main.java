8870214ec2405b38860d54e94343bd2b0d0bb7a5
Rahul Yadav <ry9064526713@gmail.com>
2026-04-07 23:24:15 +0530

    added some essential comments

--- Properties/Polymorphism/Main.java
+++ Properties/Polymorphism/Main.java
@@ -1,11 +1,13 @@
 package OOPS.Properties.Polymorphism;
-
+//it is the file from where everything is being called
 public class Main {
     public static void main(String[] args) {
         Shapes shape = new Shapes();
         Triangle triangle = new Triangle();
         Square square = new Square();
         Shapes square1 = new Square(); //Java reads the right side first
+        //but we can't do it like this
+        //Square square1 = new Shapes(); as it will give error
         //first it will create a Square constructor then call the Square constructor
         //Even before that, call Shapes constructor (inheritance rule)
         //Object Type Decides Which Constructor Runs
